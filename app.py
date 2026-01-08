33901697e87011f714cdde6f9b3467f830a9aab3
pcjansen09-max <pcjansen09@gmail.com>
2026-01-08 14:35:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,9 @@ def webhook_received():
     return '', 200
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
